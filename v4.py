4724e59eb2b239a5a5a01ac9fec6d4bc5f8ac327
Floor Harmsen <floor.harmsen@gmail.com>
2021-05-27 14:49:18 +0200

    added housing details

--- v4.py
+++ v4.py
@@ -25,6 +25,42 @@ class User:
                f"Email:\n {self.email_address}"
 
 
+class House:
+    def __init__(self, rent_mortgage, service_costs):
+        self.rent_mortgage = rent_mortgage
+        self.service_costs = service_costs
+
+    @property
+    def total_costs(self):
+        return self.rent_mortgage + self.service_costs
+
+    def __str__(self):
+        return f"Your housing details:\n" \
+               f" - rent/mortgage: €{self.rent_mortgage} \n" \
+               f" - service costs: €{self.service_costs}\n" \
+               f" - total: €{self.total_costs} per month"
+
+
+class Car:
+    def __init__(self, insurance, road_taxes, parking_permit, road_assistance):
+        self.insurance = insurance
+        self.road_taxes = road_taxes
+        self.parking_permit = parking_permit
+        self.road_assistance = road_assistance
+
+    @property
+    def total_costs(self):
+        return self.insurance + self.road_taxes + self.parking_permit + self.road_assistance
+
+    def __str__(self):
+        return f"Your car details:\n" \
+               f" - insurance: €{self.insurance}\n" \
+               f" - road taxes: €{self.road_taxes}\n" \
+               f" - parking permit: €{self.parking_permit}\n" \
+               f" - road assistance: €{self.road_assistance}\n" \
+               f" - total: {self.total_costs} per month"
+
+
 class Data:
     def __init__(self, user):
         self.user = user
@@ -47,14 +83,61 @@ class Data:
                f"{'Car: no details available.' if not self.car else self.car}"
 
 
+# --- DATA ---
+def ask_amount(question):
+    """
+    Asks the user for input on how much they pay and takes the input.
+    :param question: the question that is displayed to the user.
+    For example: How much do you pay for rent / mortgage?
+    :return: the number (cost) the user entered as an integer.
+    """
+    while True:
+        try:
+            number = int(input(question + " "))
+            if number < 0:
+                raise ValueError
+            return number
+        except ValueError:
+            print(f"Please enter a positive number (no decimals).")
+
+
+# --- ASKING THE USER FOR INPUT ---
 def user_details():
+    """
+    Function asks the user input on first name, last name and email address.
+    :return: Returns a user object with the given input.
+    """
     first_name = input("What is your first name? ")
     last_name = input("What is your last name? ")
     email_address = input("What is your email address? ")
     return User(first_name, last_name, email_address)
 
 
+def housing_details():
+    """
+    Function asks the user input for housing: rent/mortgage and service costs.
+    :return: Returns a house object with the given input.
+    """
+    rent_mortgage = ask_amount("How much do you pay for rent / mortgage?")
+    service_costs = ask_amount("How much do you pay for service costs?")
+    return House(rent_mortgage, service_costs)
+
+
+# def car_details():
+#     """
+#     Function asks the user input for the car: insurance, road taxes, parking permit and road assistance.
+#     :return: Returns a car object with the given input.
+#     """
+#     insurance = ask_amount("How much do you pay for your car insurance?")
+#     road_taxes = ask_amount("How much do you pay for road taxes?")
+#     parking_permit = cost_parking_permit()
+#     road_assistance = cost_road_assistance()
+#     return Car(insurance, road_taxes, parking_permit, road_assistance)
+
+
 if __name__ == '__main__':
     user = user_details()
     user_data = Data(user)
+    house = housing_details()
+    user_data.house = house
     print(user_data)
